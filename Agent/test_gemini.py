ab9ad5857d9c721f09ccbabfa44c9256869a50d0
KanwarAfaq <kmafaq786@gmail.com>
2026-09-10 15:27:11 +0800

    fixed

--- Agent/test_gemini.py
+++ Agent/test_gemini.py
@@ -1,8 +1,8 @@
 from google import genai
 
 # Initialize the Gemini client
-# client = genai.Client(api_key="AQ.Ab8RN6IgNIz5D-KB0O5eI9CMSLnDZjJDDeDduNueYQvcppmROg")
-client = genai.Client(api_key="AQ.Ab8RN6KPv4GwquEz0VNWrHTBoRlzhYdEwfEB9vBVFkQDIEl5Zw")
+# client = genai.Client(api_key="")
+client = genai.Client(api_key="")
 
 response = client.models.generate_content(
     model="gemini-3.8-flash",
